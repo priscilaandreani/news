2cc94d3024296ab349ee68b4135dad9a3d6ccaef
priscila andreani <priscilaandreani@gmail.com>
2023-01-28 15:29:09 -0300

    style: main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
+import styles from './home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello nexts</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for U$9.90 month</span>
+          </p>
+        </section>
+        <img src='/images/avatar.svg' alt='girl coding' />
+      </main>
     </>
   );
 }
