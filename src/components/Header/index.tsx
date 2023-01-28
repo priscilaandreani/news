a5b4a9e215403455b4b2e2574eec41dc303e0d26
priscila andreani <priscilaandreani@gmail.com>
2023-01-28 15:28:57 -0300

    feat: add signInButton to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export default function Header() {
@@ -11,9 +12,7 @@ export default function Header() {
           </a>
           <a href='#'>Posts</a>
         </nav>
-        <button type='button'>
-          <img src='/images/avatar.svg' alt='Avatar' />
-        </button>
+        <SignInButton />
       </div>
     </header>
   );
