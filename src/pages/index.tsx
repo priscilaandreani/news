716c780af215ca0817b8f9cbe13fdbdd69e9e14e
priscila andreani <priscilaandreani@gmail.com>
2023-01-28 16:27:34 -0300

    feat: ensure price to be revalidate in 24h using static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import styles from './home.module.scss';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 
 interface HomeProps {
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1MVJVDKs1cziZKvIIMgbsOkz');
 
   const product = {
@@ -51,5 +51,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
