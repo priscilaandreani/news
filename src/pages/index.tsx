60c65574a8f829a860c573b88a1b60786bd43532
priscila andreani <priscilaandreani@gmail.com>
2023-01-28 16:10:09 -0300

    style: add subscribe button to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from './home.module.scss';
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
           <p>
             Get access to all the publications <br />
             <span>for U$9.90 month</span>
+            <SubscribeButton />
           </p>
         </section>
         <img src='/images/avatar.svg' alt='girl coding' />
